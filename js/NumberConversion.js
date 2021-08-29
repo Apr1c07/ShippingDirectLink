c2e2872aa756d8ea90a2854f89088832eb594ff0
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2021-08-29 18:26:12 +0900

    Update NumberConversion.js

--- js/NumberConversion.js
+++ js/NumberConversion.js
@@ -1,22 +1,23 @@
 function URLConversion() {
-  alert('4');
+  alert('5');
+  document.getElementById("inputNumber").value = ""
   if(document.getElementById("inputNumber").value != "")
   {
-    document.getElementById("inputNumber").value = ""
     var inputNumber = document.getElementById('inputNumber').value;
     var elements = document.getElementsByName('CoName');
     let checkValue = '';
     for (let i = 0; i < elements.length; i++) {
-      alert(i+'番目  '+elements[i].checked+'　'+elements[i].value);
       if (elements[i].checked) {
         checkValue = elements[i].value;
       }
     }
+    alert(checkValue)
     var Yamato = "https://jizen.kuronekoyamato.co.jp/jizen/servlet/crjz.b.NQ0010?id=";
     var Sagawa = "https://k2k.sagawa-exp.co.jp/p/web/okurijosearch.do?okurijoNo=";
     var Yubin = "https://trackings.post.japanpost.jp/services/srv/search/direct?locale=ja&reqCodeNo1=";
     var a = inputNumber.replace("-", "");
     var b = ""
+    alert(checkValue)
     if (checkValue = "0") {
       alert('in 0')
       b = Yamato + a
@@ -27,6 +28,7 @@ function URLConversion() {
       alert('in 2')
       b = Yubin + a
     }
+    alert(checkValue)
     document.getElementById("inputNumber").value = b;
   }
 }
